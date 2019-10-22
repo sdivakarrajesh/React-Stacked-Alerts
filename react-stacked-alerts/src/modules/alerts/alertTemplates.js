4c2a23745ffda8f14d73ed8b8d8fec2edbf69c0d
Divakar Rajesh <sdivakarrajesh@gmail.com>
2019-10-22 14:38:58 +0530

    added shaded alert template

--- react-stacked-alerts/src/modules/alerts/alertTemplates.js
+++ react-stacked-alerts/src/modules/alerts/alertTemplates.js
@@ -11,8 +11,26 @@ const LabelledAlert = (props) => {
     </div>
 }
 
+const ShadedAlert = (props) => {
+
+    let alertStyle = { 
+        backgroundColor: props.label_color ? props.label_color : 'green',
+        borderLeft: props.border_right_color ? `3px solid ${props.border_right_color}`: '3px solid darkgreen'
+    }
+    let labelStyle = {
+        color: props.border_right_color ? props.border_right_color : 'darkgreen'
+    }
+    
+
+    return <div className='labelled-alert' style={alertStyle}>
+        <div className='shaded-alert-label' style={labelStyle}>{props.label}</div>
+        <div className='shaded-alert-content'>{props.alert_content}</div>
+    </div>
+}
+
 
 
 export {
     LabelledAlert,
+    ShadedAlert,
 }
